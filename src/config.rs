3badb9e224285990fe63f78d6ca62fb306caadc1
EliseZeroTwo <elise@headpat.services>
2021-01-08 00:13:44 +0100

    Add keyboard input + config

---
+++ src/config.rs
@@ -0,0 +1,124 @@
+use directories::ProjectDirs;
+use sdl2::keyboard::Scancode;
+use serde::{Serialize, Deserialize};
+use std::fs;
+
+// Lin: /home/elise/.config/WARS-8/config.json
+// Win: C:\Users\elise\AppData\Roaming\headpat\WARS-8\config\config.json
+// Mac: /Users/elise/Library/Application Support/services.headpat.WARS-8/config.json
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PlayerKeyBindings {
+    pub left: i32,
+    pub right: i32,
+    pub up: i32,
+    pub down: i32,
+    pub o: i32,
+    pub x: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct KeyBindings {
+    pub player1: PlayerKeyBindings,
+    pub player2: PlayerKeyBindings,
+    pub quit: i32,
+    pub pause: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct VideoSettings {
+    pub fullscreen: bool,
+    pub borderless: bool,
+    pub fps: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub video: VideoSettings,
+    pub keys: KeyBindings,
+}
+
+impl Config {
+    pub fn new() -> Config {
+        Config {
+            video: VideoSettings {
+                fullscreen: false,
+                borderless: false,
+                fps: true,
+            },
+            keys: KeyBindings {
+                player1: PlayerKeyBindings {
+                    left: Scancode::Left as i32,
+                    right: Scancode::Right as i32,
+                    up: Scancode::Up as i32,
+                    down: Scancode::Down as i32,
+                    o: Scancode::Z as i32,
+                    x: Scancode::X as i32,
+                },
+                player2: PlayerKeyBindings {
+                    left: Scancode::S as i32,
+                    right: Scancode::F as i32,
+                    up: Scancode::E as i32,
+                    down: Scancode::D as i32,
+                    o: Scancode::Tab as i32,
+                    x: Scancode::Q as i32,
+                },
+                quit: Scancode::Escape as i32,
+                pause: Scancode::P as i32,
+            }
+        }
+    }
+
+    pub fn get_config_or_create() -> Config {
+        let mut config: Config = Config::new();
+        if let Some(proj_dirs) = ProjectDirs::from("services", "headpat",  "WARS-8") {
+            let path = proj_dirs.config_dir();
+            if !path.exists() {
+                if let Err(why) = fs::create_dir_all(path) {
+                    panic!("Not able to create config directory! Reason: {}", why);
+                }
+            }
+
+            let path = path.join("config.json");
+            if !path.exists() {
+                if let Err(why) = fs::write(path, serde_json::to_string_pretty(&config).unwrap()) {
+                    panic!("Not able to create config! Reason: {}", why);
+                }
+            } else {
+                match fs::read_to_string(&path){
+                    Ok(content) => {
+                        match serde_json::from_str(&content) {
+                            Ok(cfg) => config = cfg,
+                            Err(_) => {
+                                fs::remove_file(&path).unwrap();
+                                if let Err(why) = fs::write(&path, serde_json::to_string_pretty(&config).unwrap()) {
+                                    panic!("Not able to create config! Reason: {}", why);
+                                }
+                            },
+                        }
+                    },
+                    Err(why) => panic!("Unable to read config file! Reason: {}", why),
+                }
+            }
+        };
+        config
+    }
+
+    pub fn save_config(&self) {
+        if let Some(proj_dirs) = ProjectDirs::from("services", "headpat",  "WARS-8") {
+            let path = proj_dirs.config_dir();
+            if !path.exists() {
+                if let Err(why) = fs::create_dir_all(path) {
+                    panic!("Not able to create config directory! Reason: {}", why);
+                }
+            }
+
+            let path = path.join("config.json");
+            fs::remove_file(&path).unwrap();
+
+            if let Err(why) = fs::write(path, serde_json::to_string_pretty(self).unwrap()) {
+                panic!("Not able to create config! Reason: {}", why);
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+extern crate directories;
 #[macro_use]
 extern crate lazy_static;
-extern crate wasmtime;
 extern crate sdl2;
+extern crate serde_json;
+extern crate wasmtime;
 
 macro_rules! func_wrap {
     ($wasm_runtime:expr, $func:expr) => {
@@ -9,27 +11,27 @@ macro_rules! func_wrap {
     };
 }
 
+mod config;
 mod fps_counter;
 mod palette;
 mod runtime;
 mod wasm_runtime;
 
+use crate::config::Config;
 use crate::fps_counter::FpsCounter;
 use crate::palette::ColorPallete;
 use crate::wasm_runtime::WasmRuntime;
 use crate::runtime::*;
 use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
+use sdl2::keyboard::Scancode;
 use sdl2::pixels::Color;
 use sdl2::pixels::PixelFormatEnum;
 use std::convert::From;
-use std::collections::HashMap;
 use std::sync::Mutex;
 use wasmtime::*;
 
 const WIDTH: i32 = 256;
 const HEIGHT: i32 = 256;
-const RUNTIME: Runtimes = Runtimes::Wasm;
 const TARGET_FPS: f32 = 30.0;
 const FRAME_LEN_MS: u32 = ((1.0 / TARGET_FPS) * 1000.0) as u32; 
 
@@ -42,8 +44,42 @@ impl From<TerminalLocation> for usize {
     }
 }
 
+#[derive(Copy, Clone, Debug)]
+pub struct KeyState {
+    left: bool,
+    right: bool,
+    up: bool,
+    down: bool,
+    o: bool,
+    x: bool,
+}
+
+impl KeyState {
+    pub fn new() -> KeyState {
+        KeyState {
+            left: false,
+            right: false,
+            up: false,
+            down: false,
+            o: false,
+            x: false,
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.left = false;
+        self.right = false;
+        self.up = false;
+        self.down = false;
+        self.o = false;
+        self.x = false;
+    }
+}
+
 lazy_static! {
     static ref PXBUF_MUTEX: Mutex<[ColorPallete; (WIDTH * HEIGHT) as usize]> = Mutex::new([ColorPallete::Black; (WIDTH * HEIGHT) as usize]);
+    static ref KEYSTATE_FRAME: Mutex<[KeyState; 2]> = Mutex::new([KeyState::new(); 2]);
+    static ref KEYSTATE_HELD: Mutex<[KeyState; 2]> = Mutex::new([KeyState::new(); 2]);
 }
 
 fn pset(x: i32, y: i32, color: i32) {
@@ -88,12 +124,55 @@ fn rectfill(x0: i32, y0: i32, x1: i32, y1: i32, color: i32) {
     }
 }
 
+fn cls(color: i32) {
+    rectfill(0, 0, WIDTH - 1, HEIGHT - 1, color);
+}
+
+fn btn(i: i32, p: i32) -> i32 {
+    let keystate_held = KEYSTATE_HELD.lock().unwrap();
+    if p != 1 && p != 2 {
+        panic!("Invalid player number {}", p);
+    }
+    let idx = (p - 1) as usize;
+    let val = match i {
+        0 => keystate_held[idx].left,
+        1 => keystate_held[idx].right,
+        2 => keystate_held[idx].up,
+        3 => keystate_held[idx].down,
+        4 => keystate_held[idx].o,
+        5 => keystate_held[idx].x,
+        _ => panic!("Invalid keycode {}", i),
+    };
+    
+    val as i32
+}
+
+fn btnp(i: i32, p: i32) -> i32 {
+    let keystate_frame = KEYSTATE_FRAME.lock().unwrap();
+    if p != 1 && p != 2 {
+        panic!("Invalid player number {}", p);
+    }
+    let idx = (p - 1) as usize;
+    let val = match i {
+        0 => keystate_frame[idx].left,
+        1 => keystate_frame[idx].right,
+        2 => keystate_frame[idx].up,
+        3 => keystate_frame[idx].down,
+        4 => keystate_frame[idx].o,
+        5 => keystate_frame[idx].x,
+        _ => panic!("Invalid keycode {}", i),
+    };
+    
+    val as i32
+}
 
 fn main() {
     if std::env::args().len() == 1 {
         println!("Usage: {} <Binary>", std::env::args().next().unwrap());
         std::process::exit(1);
     }
+
+    let config = Config::get_config_or_create();
 
     let mut path = "".to_owned();
     for x in std::env::args() {
@@ -117,10 +196,13 @@ fn main() {
     let mut missing_import_vec: Vec<String> = Vec::new();
     for import in runtime.module.as_ref().unwrap().imports() {
         match import.name() {
+            "cls" => import_vec.push(func_wrap!(runtime, cls)),
             "pset" => import_vec.push(func_wrap!(runtime, pset)),
             "pget" => import_vec.push(func_wrap!(runtime, pget)),
             "rect" => import_vec.push(func_wrap!(runtime, rect)),
             "rectfill" => import_vec.push(func_wrap!(runtime, rectfill)),
+            "btn" => import_vec.push(func_wrap!(runtime, btn)),
+            "btnp" => import_vec.push(func_wrap!(runtime, btnp)),
             _ => missing_import_vec.push(import.name().to_owned()),
         }
         println!("Attempting to import {}", import.name());
@@ -134,19 +216,10 @@ fn main() {
 
     let mut target_ms = sdl_ctx.timer().unwrap().ticks() + FRAME_LEN_MS;
     let mut fps_counter = FpsCounter::new(sdl_ctx.timer().unwrap().ticks());
+    let mut paused = false;
 
     runtime.init();
     'sdlloop: loop {
-        for event in sdl_ctx.event_pump().unwrap().poll_iter() {
-            match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'sdlloop
-                },
-                _ => { }
-            }
-        }
-
         runtime.update();
         runtime.draw();
 
@@ -163,6 +236,48 @@ fn main() {
 
         canvas.copy(&texture, None, None).unwrap();
         canvas.present();
+
+        {
+            let mut keystate_frame = KEYSTATE_FRAME.lock().unwrap();
+            keystate_frame[0].reset();
+            keystate_frame[1].reset();
+            let mut keystate_held = KEYSTATE_HELD.lock().unwrap();
+            for event in sdl_ctx.event_pump().unwrap().poll_iter() {
+                match event {
+                    Event::Quit {..} => { break 'sdlloop; },
+                    Event::KeyDown { scancode: Some(kc), .. } if kc as i32 == config.keys.quit => { break 'sdlloop; },
+                    Event::KeyDown { scancode: Some(kc), .. } if kc as i32 == config.keys.pause => { paused = !paused; },
+
+                    Event::KeyDown { scancode: Some(kc), .. } if kc as i32 == config.keys.player1.left => { keystate_frame[0].left = true; keystate_held[0].left = true; },
+                    Event::KeyDown { scancode: Some(kc), .. } if kc as i32 == config.keys.player1.right => { keystate_frame[0].right = true; keystate_held[0].right = true; },
+                    Event::KeyDown { scancode: Some(kc), .. } if kc as i32 == config.keys.player1.up => { keystate_frame[0].up = true; keystate_held[0].up = true; },
+                    Event::KeyDown { scancode: Some(kc), .. } if kc as i32 == config.keys.player1.down => { keystate_frame[0].down = true; keystate_held[0].down = true; },
+                    Event::KeyDown { scancode: Some(kc), .. } if kc as i32 == config.keys.player1.o => { keystate_frame[0].o = true; keystate_held[0].o = true; },
+                    Event::KeyDown { scancode: Some(kc), .. } if kc as i32 == config.keys.player1.x => { keystate_frame[0].x = true; keystate_held[0].x = true; },
+                    Event::KeyDown { scancode: Some(kc), .. } if kc as i32 == config.keys.player2.left => { keystate_frame[1].left = true; keystate_held[1].left = true; },
+                    Event::KeyDown { scancode: Some(kc), .. } if kc as i32 == config.keys.player2.right => { keystate_frame[1].right = true; keystate_held[1].right = true; },
+                    Event::KeyDown { scancode: Some(kc), .. } if kc as i32 == config.keys.player2.up => { keystate_frame[1].up = true; keystate_held[1].up = true; },
+                    Event::KeyDown { scancode: Some(kc), .. } if kc as i32 == config.keys.player2.down => { keystate_frame[1].down = true; keystate_held[1].down = true; },
+                    Event::KeyDown { scancode: Some(kc), .. } if kc as i32 == config.keys.player2.o => { keystate_frame[1].o = true; keystate_held[1].o = true; },
+                    Event::KeyDown { scancode: Some(kc), .. } if kc as i32 == config.keys.player2.x => { keystate_frame[1].x = true; keystate_held[1].x = true; },
+                    
+                    Event::KeyUp { scancode: Some(kc), .. } if kc as i32 == config.keys.player1.left => { keystate_held[0].left = false; },
+                    Event::KeyUp { scancode: Some(kc), .. } if kc as i32 == config.keys.player1.right => { keystate_held[0].right = false; },
+                    Event::KeyUp { scancode: Some(kc), .. } if kc as i32 == config.keys.player1.up => { keystate_held[0].up = false; },
+                    Event::KeyUp { scancode: Some(kc), .. } if kc as i32 == config.keys.player1.down => { keystate_held[0].down = false; },
+                    Event::KeyUp { scancode: Some(kc), .. } if kc as i32 == config.keys.player1.o => { keystate_held[0].o = false; },
+                    Event::KeyUp { scancode: Some(kc), .. } if kc as i32 == config.keys.player1.x => { keystate_held[0].x = false; },
+                    Event::KeyUp { scancode: Some(kc), .. } if kc as i32 == config.keys.player2.left => { keystate_held[1].left = false; },
+                    Event::KeyUp { scancode: Some(kc), .. } if kc as i32 == config.keys.player2.right => { keystate_held[1].right = false; },
+                    Event::KeyUp { scancode: Some(kc), .. } if kc as i32 == config.keys.player2.up => { keystate_held[1].up = false; },
+                    Event::KeyUp { scancode: Some(kc), .. } if kc as i32 == config.keys.player2.down => { keystate_held[1].down = false; },
+                    Event::KeyUp { scancode: Some(kc), .. } if kc as i32 == config.keys.player2.o => { keystate_held[1].o = false; },
+                    Event::KeyUp { scancode: Some(kc), .. } if kc as i32 == config.keys.player2.x => { keystate_held[1].x = false; },
+
+                    _ => { }
+                }
+            }
+        }
 
         let frame_difference = target_ms as i32 - sdl_ctx.timer().unwrap().ticks() as i32;
         target_ms += FRAME_LEN_MS;
